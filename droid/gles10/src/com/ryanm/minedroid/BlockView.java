a7c9072cba41ffa3a4ad86195773964c32203bfa
therealryan <therealryan@580138e2-45db-8bd6-c95b-24c1a288ad4b>
2010-12-29 18:06:09 +0000

    For GLes 1.0 phones. Won't be maintained

---
+++ droid/gles10/src/com/ryanm/minedroid/BlockView.java
@@ -0,0 +1,343 @@
+
+package com.ryanm.minedroid;
+
+import static android.opengl.GLES10.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES10.GL_DEPTH_BUFFER_BIT;
+import static android.opengl.GLES10.glClear;
+import android.opengl.GLES10;
+import android.util.FloatMath;
+import android.view.KeyEvent;
+
+import com.ryanm.droid.config.annote.Category;
+import com.ryanm.droid.config.annote.Summary;
+import com.ryanm.droid.config.annote.Variable;
+import com.ryanm.droid.config.annote.WidgetHint;
+import com.ryanm.droid.rugl.Game;
+import com.ryanm.droid.rugl.Phase;
+import com.ryanm.droid.rugl.input.TapPad;
+import com.ryanm.droid.rugl.util.Colour;
+import com.ryanm.droid.rugl.util.FPSCamera;
+import com.ryanm.droid.rugl.util.geom.BoundingCuboid;
+import com.ryanm.droid.rugl.util.geom.Vector3f;
+import com.ryanm.minedroid.BlockFactory.Block;
+
+/**
+ * @author ryanm
+ */
+@Variable
+@Summary( "Explore minecraft worlds" )
+public class BlockView extends Phase
+{
+	/***/
+	@Variable
+	public GUI gui;
+
+	/***/
+	@Variable
+	public FPSCamera cam = new FPSCamera();
+
+	/***/
+	@Variable( "Sky colour" )
+	@WidgetHint( Colour.class )
+	public int skyColour = Colour.packFloat( 0.7f, 0.7f, 0.9f, 1 );
+
+	/***/
+	@Variable( "Speed" )
+	@Summary( "In blocks per second" )
+	@Category( "Motion" )
+	public float speed = 4f;
+
+	/***/
+	@Variable( "Jump speed" )
+	@Summary( "Vertical speed on jumping, in blocks per second" )
+	@Category( "Motion" )
+	public float jumpSpeed = 6f;
+
+	/***/
+	@Variable( "Gravity" )
+	@Summary( "Acceleration due to gravity, in blocks per second per second" )
+	@Category( "Motion" )
+	public float gravity = -10;
+
+	/***/
+	@Variable( "Ghost mode" )
+	@Summary( "Fly free or suffer a mundane corporeal existence" )
+	public boolean ghost = false;
+
+	/***/
+	@Variable( "Width" )
+	@Summary( "Width of character, in block units" )
+	@Category( "Clipping" )
+	public float width = 0.75f;
+
+	/***/
+	@Variable( "Height" )
+	@Summary( "Height of character, in block units" )
+	@Category( "Clipping" )
+	public float height = 1.8f;
+
+	/***/
+	@Variable( "Eye level" )
+	@Summary( "The height of the camera, in terms of character height" )
+	@Category( "Clipping" )
+	public float eyeLevel = 0.9f;
+
+	/***/
+	@Variable( "Crouched eye level" )
+	@Summary( "The height of the camera when crouched, in terms of character height" )
+	@Category( "Clipping" )
+	public float crouchedEyeLevel = 0.65f;
+
+	private boolean onGround = false;
+
+	private boolean crouched = false;
+
+	private Vector3f position = new Vector3f();
+
+	private Vector3f velocity = new Vector3f();
+
+	// handy data structures for collision detection
+	private Vector3f collideCorrection = new Vector3f();
+
+	private BoundingCuboid playerBounds = new BoundingCuboid( 0, 0, 0, 0, 0, 0 );
+
+	private BoundingCuboid blockBounds = new BoundingCuboid( 0, 0, 0, 0, 0, 0 );
+
+	private BoundingCuboid intersection = new BoundingCuboid( 0, 0, 0, 0, 0, 0 );
+
+	/***/
+	@Variable
+	public final World world;
+
+	private Game game;
+
+	private TapPad.Listener jumpCrouchListener = new TapPad.Listener() {
+		@Override
+		public void onTap( TapPad pad )
+		{
+			if( crouched )
+			{
+				crouched = false;
+			}
+			else if( onGround )
+			{
+				velocity.y = jumpSpeed;
+			}
+		}
+
+		@Override
+		public void onLongPress( TapPad pad )
+		{
+			crouched = true;
+		}
+	};
+
+	/**
+	 * @param world
+	 */
+	public BlockView( World world )
+	{
+		this.world = world;
+		resetLocation();
+	}
+
+	/***/
+	@Variable( "Reset location" )
+	@Summary( "Lost? Go back to your starting location" )
+	public void resetLocation()
+	{
+		position.set( world.startPosition );
+		velocity.set( 0, 0, 0 );
+	}
+
+	@Override
+	public void init( Game game )
+	{
+		Game.setConfigurationRoots( game, this );
+		Game.logicAdvance = 1.0f / 60;
+
+		this.game = game;
+
+		cam.far = 80;
+
+		if( gui == null )
+		{
+			gui = new GUI();
+			gui.rightTap.listener = jumpCrouchListener;
+		}
+
+		BlockFactory.loadTexture();
+	}
+
+	@Override
+	public void openGLinit()
+	{
+		GLES10.glClearColor( Colour.redf( skyColour ), Colour.greenf( skyColour ),
+				Colour.bluef( skyColour ), Colour.alphaf( skyColour ) );
+	}
+
+	@Override
+	public void advance( float delta )
+	{
+		gui.advance( delta );
+
+		cam.advance( delta, gui.right.x, gui.right.y );
+		position.x += gui.left.y * delta * cam.forward.x * speed;
+		position.z += gui.left.y * delta * cam.forward.z * speed;
+
+		position.x += -gui.left.x * delta * cam.right.x * speed;
+		position.z += -gui.left.x * delta * cam.right.z * speed;
+
+		if( ghost )
+		{
+			position.y += gui.left.y * delta * cam.forward.y * speed;
+			position.y += -gui.left.x * delta * cam.right.y * speed;
+
+			velocity.y = 0;
+		}
+		else if( world.getChunklet( position.x, position.y, position.z ) != null )
+		{ // make sure the chunk we're in is loaded first
+
+			// gravity
+			velocity.y += gravity * delta;
+			position.y += velocity.y * delta;
+
+			// world collide
+			float w = width / 2;
+			float feet = height * ( crouched ? crouchedEyeLevel : eyeLevel );
+			float head = height - feet;
+			playerBounds.set( position.x - w, position.y - feet, position.z - w, position.x
+					+ w, position.y + head, position.z + w );
+
+			boolean groundHit = false;
+
+			for( float x = FloatMath.floor( playerBounds.x.getMin() ); x < playerBounds.x
+					.getMax(); x++ )
+			{
+				for( float z = FloatMath.floor( playerBounds.z.getMin() ); z < playerBounds.z
+						.getMax(); z++ )
+				{
+					for( float y = FloatMath.floor( playerBounds.y.getMin() ); y < playerBounds.y
+							.getMax(); y++ )
+					{
+						collideCorrection.set( 0, 0, 0 );
+
+						collide( x, y, z, playerBounds, collideCorrection );
+
+						playerBounds.translate( collideCorrection.x, collideCorrection.y,
+								collideCorrection.z );
+						Vector3f.add( position, collideCorrection, position );
+
+						if( collideCorrection.y != 0
+								&& Math.signum( collideCorrection.y ) != Math.signum( velocity.y ) )
+						{ // hit the ground or roof
+							velocity.y = 0;
+						}
+
+						groundHit |= collideCorrection.y > 0;
+					}
+				}
+			}
+
+			onGround = groundHit;
+		}
+
+		world.advance( position.x, position.z );
+	}
+
+	/**
+	 * Collides a point against the world's blocks, computes the
+	 * smallest correction to rectify any collision
+	 * 
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @param correction
+	 */
+	private void collide( float x, float y, float z, BoundingCuboid player,
+			Vector3f correction )
+	{
+		byte bt = world.blockType( x, y, z );
+		Block b = BlockFactory.getBlock( bt );
+
+		if( !( b == null || b == Block.Water || b == Block.StillWater ) )
+		{
+			x = FloatMath.floor( x );
+			y = FloatMath.floor( y );
+			z = FloatMath.floor( z );
+			blockBounds.set( x, y, z, x + 1, y + 1, z + 1 );
+
+			if( playerBounds.intersection( blockBounds, intersection ) )
+			{
+				correction( intersection, collideCorrection );
+
+				// Log.i( Game.RUGL_TAG, "Eye at " + position );
+				// Log.i( Game.RUGL_TAG, "Player bounds " + player );
+				// Log.i( Game.RUGL_TAG, "Block " + blockBounds.toString()
+				// );
+				// Log.i( Game.RUGL_TAG, "intersection " +
+				// intersection.toString() );
+				// Log.i( Game.RUGL_TAG, "Correction " +
+				// collideCorrection.toString() );
+			}
+		}
+	}
+
+	/**
+	 * Calculates the minimum correction vector to move the point out
+	 * of the unit cube
+	 * 
+	 * @param intersection
+	 * @param correction
+	 */
+	private void correction( BoundingCuboid intersection, Vector3f correction )
+	{
+		float mx = intersection.x.getSpan();
+		float my = intersection.y.getSpan();
+		float mz = intersection.z.getSpan();
+
+		if( mx < my && mx < mz )
+		{
+			correction.set( intersection.x.toValue( 0.5f ) < position.x ? mx : -mx, 0, 0 );
+		}
+		else if( my < mz )
+		{
+			correction.set( 0, intersection.y.toValue( 0.5f ) < position.y ? my : -my, 0 );
+		}
+		else
+		{
+			correction.set( 0, 0, intersection.z.toValue( 0.5f ) < position.z ? mz : -mz );
+		}
+	}
+
+	@Override
+	public void draw()
+	{
+		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+		cam.setPosition( position.x, position.y, position.z );
+
+		world.draw( position, cam.frustum );
+
+		gui.draw();
+	}
+
+	@Override
+	public Phase next()
+	{
+		return null;
+	}
+
+	@Override
+	public void onKeyDown( int keyCode, KeyEvent event )
+	{
+		if( keyCode == KeyEvent.KEYCODE_BACK )
+		{
+			complete = true;
+		}
+		else if( keyCode == KeyEvent.KEYCODE_MENU )
+		{
+			game.launchConfiguration();
+		}
+	}
+}
---
+++ droid/gles10/src/com/ryanm/minedroid/World.java
@@ -0,0 +1,559 @@
+
+package com.ryanm.minedroid;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import android.util.Log;
+
+import com.ryanm.droid.config.annote.Summary;
+import com.ryanm.droid.config.annote.Variable;
+import com.ryanm.droid.rugl.Game;
+import com.ryanm.droid.rugl.gl.GLUtil;
+import com.ryanm.droid.rugl.gl.MutableState;
+import com.ryanm.droid.rugl.gl.Renderer;
+import com.ryanm.droid.rugl.res.ResourceLoader;
+import com.ryanm.droid.rugl.util.QuickSort;
+import com.ryanm.droid.rugl.util.geom.Frustum;
+import com.ryanm.droid.rugl.util.geom.Frustum.Result;
+import com.ryanm.droid.rugl.util.geom.ReadableVector3f;
+import com.ryanm.droid.rugl.util.geom.Vector3f;
+
+/**
+ * Manages loading chunks, decides which chunklets to render
+ * 
+ * @author ryanm
+ */
+@Variable( "World" )
+@Summary( "Controls chunk loading and render state" )
+public class World
+{
+	/***/
+	@Variable( "Render state" )
+	public MutableState muState;
+
+	/***/
+	@Variable( "Draw chunklets" )
+	public boolean drawChunklets = true;
+
+	/***/
+	@Variable( "Outline chunklets" )
+	public boolean drawOutlines = false;
+
+	/**
+	 * For drawing the wireframes
+	 */
+	private Renderer renderer = new Renderer();
+
+	/**
+	 * The world save directory
+	 */
+	public final File dir;
+
+	private int loadradius = 4;
+
+	/**
+	 * 1st index = x, 2nd = z
+	 */
+	private Chunk[][] chunks =
+			new Chunk[ 2 * getLoadRadius() + 1 ][ 2 * getLoadRadius() + 1 ];
+
+	/**
+	 * Coordinates of the currently-occupied chunk
+	 */
+	private int chunkPosX, chunkPosZ;
+
+	private Queue<Chunklet> floodQueue = new ArrayBlockingQueue<Chunklet>( 50 );
+
+	// we're not using ArrayList here because Collections.sort creates
+	// garbage. Hopefully Arrays.sort does not
+	private Chunklet[] renderList = new Chunklet[ 64 ];
+
+	private int renderListSize = 0;
+
+	/**
+	 * Number of chunklets rendered in the last draw call
+	 */
+	public static int renderedChunklets = 0;
+
+	private int drawFlag = Integer.MIN_VALUE;
+
+	/**
+	 * The player position, as read from level.dat
+	 */
+	public final ReadableVector3f startPosition;
+
+	private static final ChunkSorter cs = new ChunkSorter();
+
+	/**
+	 * @param dir
+	 * @param startPosition
+	 */
+	public World( File dir, Vector3f startPosition )
+	{
+		this.dir = dir;
+		this.startPosition = startPosition;
+
+		chunkPosX = ( int ) startPosition.getX() / 16;
+		chunkPosZ = ( int ) startPosition.getZ() / 16;
+
+		Game.addSurfaceLIstener( new Game.SurfaceListener() {
+			@Override
+			public void onSurfaceCreated()
+			{
+				setLoadRadius( loadradius );
+			};
+		} );
+	}
+
+	/**
+	 * Tries to ensure we have the right chunks loaded
+	 * 
+	 * @param posX
+	 *           player position
+	 * @param posZ
+	 *           player position
+	 */
+	public void advance( float posX, float posZ )
+	{
+		boolean chunksDirty = false;
+
+		int cx = ( int ) Math.floor( posX / 16 );
+		if( cx < chunkPosX )
+		{
+			shiftUpX();
+			chunkPosX--;
+			chunksDirty = true;
+		}
+		else if( cx > chunkPosX )
+		{
+			shiftDownX();
+			chunkPosX++;
+			chunksDirty = true;
+		}
+
+		int cz = ( int ) Math.floor( posZ / 16 );
+		if( cz < chunkPosZ )
+		{
+			shiftUpZ();
+			chunkPosZ--;
+			chunksDirty = true;
+		}
+		else if( cz > chunkPosZ )
+		{
+			shiftDownZ();
+			chunkPosZ++;
+			chunksDirty = true;
+		}
+
+		if( chunksDirty )
+		{ // load new chunks
+			Log.i( Game.RUGL_TAG, "Entered chunk " + chunkPosX + ", " + chunkPosZ );
+
+			fillChunks();
+		}
+	}
+
+	private void shiftDownX()
+	{
+		// free bottom x
+		Chunk[] swap = chunks[ 0 ];
+		for( int i = 0; i < swap.length; i++ )
+		{
+			if( swap[ i ] != null )
+			{
+				swap[ i ].unload();
+				swap[ i ] = null;
+			}
+		}
+
+		// shift down
+		for( int i = 0; i < chunks.length - 1; i++ )
+		{
+			chunks[ i ] = chunks[ i + 1 ];
+		}
+
+		// add swap
+		chunks[ chunks.length - 1 ] = swap;
+	}
+
+	private void shiftUpX()
+	{
+		// free top x
+		Chunk[] swap = chunks[ chunks.length - 1 ];
+		for( int i = 0; i < swap.length; i++ )
+		{
+			if( swap[ i ] != null )
+			{
+				swap[ i ].unload();
+				swap[ i ] = null;
+			}
+		}
+
+		// shift up
+		for( int i = chunks.length - 1; i > 0; i-- )
+		{
+			chunks[ i ] = chunks[ i - 1 ];
+		}
+
+		// add swap
+		chunks[ 0 ] = swap;
+	}
+
+	private void shiftDownZ()
+	{
+		for( int i = 0; i < chunks.length; i++ )
+		{
+			if( chunks[ i ][ 0 ] != null )
+			{
+				chunks[ i ][ 0 ].unload();
+			}
+
+			for( int j = 0; j < chunks[ i ].length - 1; j++ )
+			{
+				chunks[ i ][ j ] = chunks[ i ][ j + 1 ];
+			}
+
+			chunks[ i ][ chunks[ i ].length - 1 ] = null;
+		}
+	}
+
+	private void shiftUpZ()
+	{
+		for( int i = 0; i < chunks.length; i++ )
+		{
+			if( chunks[ i ][ chunks[ i ].length - 1 ] != null )
+			{
+				chunks[ i ][ chunks[ i ].length - 1 ].unload();
+			}
+
+			for( int j = chunks[ i ].length - 1; j > 0; j-- )
+			{
+				chunks[ i ][ j ] = chunks[ i ][ j - 1 ];
+			}
+
+			chunks[ i ][ 0 ] = null;
+		}
+	}
+
+	/**
+	 * @param eye
+	 * @param frustum
+	 */
+	public void draw( Vector3f eye, Frustum frustum )
+	{
+		if( muState == null )
+		{
+			muState = new MutableState( BlockFactory.state );
+		}
+
+		if( muState.dirty )
+		{ // the rendering state has been changed by configuration
+			BlockFactory.state = muState.compile();
+			muState.dirty = false;
+		}
+
+		Chunklet c = getChunklet( eye.x, eye.y, eye.z );
+
+		if( c != null )
+		{
+			Chunklet origin = c;
+			c.drawFlag = drawFlag;
+			floodQueue.offer( c );
+
+			while( !floodQueue.isEmpty() )
+			{
+				c = floodQueue.poll();
+
+				renderList[ renderListSize++ ] = c;
+
+				if( renderListSize >= renderList.length )
+				{ // grow
+					Chunklet[] nrl = new Chunklet[ renderList.length * 2 ];
+					System.arraycopy( renderList, 0, nrl, 0, renderList.length );
+					renderList = nrl;
+				}
+
+				// floodfill - for each neighbouring chunklet...
+				if( c.x <= origin.x && !c.northSheet )
+				// we are not reversing flood direction and we can see
+				// through that face of this chunk
+				{
+					Chunklet north = getChunklet( c.x - 16, c.y, c.z );
+					if( north != null
+					// neighbouring chunk exists
+							&& !north.southSheet
+							// we can see through the traversal face
+							&& north.drawFlag != drawFlag
+							// we haven't already visited it in this frame
+							&& north.intersection( frustum ) != Result.Miss )
+					// it intersects the frustum
+					{
+						north.drawFlag = drawFlag;
+						floodQueue.offer( north );
+					}
+				}
+				if( c.x >= origin.x && !c.southSheet )
+				{
+					Chunklet south = getChunklet( c.x + 16, c.y, c.z );
+					if( south != null && !south.northSheet && south.drawFlag != drawFlag
+							&& south.intersection( frustum ) != Result.Miss )
+					{
+						south.drawFlag = drawFlag;
+						floodQueue.offer( south );
+					}
+				}
+				if( c.z <= origin.z && !c.eastSheet )
+				{
+					Chunklet east = getChunklet( c.x, c.y, c.z - 16 );
+					if( east != null && !east.westSheet && east.drawFlag != drawFlag
+							&& east.intersection( frustum ) != Result.Miss )
+					{
+						east.drawFlag = drawFlag;
+						floodQueue.offer( east );
+					}
+				}
+				if( c.z >= origin.z && !c.westSheet )
+				{
+					Chunklet west = getChunklet( c.x, c.y, c.z + 16 );
+					if( west != null && !west.eastSheet && west.drawFlag != drawFlag
+							&& west.intersection( frustum ) != Result.Miss )
+					{
+						west.drawFlag = drawFlag;
+						floodQueue.offer( west );
+					}
+				}
+				if( c.y <= origin.y && !c.bottomSheet )
+				{
+					Chunklet bottom = getChunklet( c.x, c.y - 16, c.z );
+					if( bottom != null && !bottom.topSheet && bottom.drawFlag != drawFlag
+							&& bottom.intersection( frustum ) != Result.Miss )
+					{
+						bottom.drawFlag = drawFlag;
+						floodQueue.offer( bottom );
+					}
+				}
+				if( c.y >= origin.y && !c.topSheet )
+				{
+					Chunklet top = getChunklet( c.x, c.y + 16, c.z );
+					if( top != null && !top.bottomSheet && top.drawFlag != drawFlag
+							&& top.intersection( frustum ) != Result.Miss )
+					{
+						top.drawFlag = drawFlag;
+						floodQueue.offer( top );
+					}
+				}
+			}
+		}
+
+		renderedChunklets = 0;
+		if( drawChunklets )
+		{
+			// sort chunklets into ascending order of distance from the
+			// eye
+			cs.eye.set( eye );
+			QuickSort.sort( renderList, cs, 0, renderListSize - 1 );
+
+			GLUtil.checkGLError();
+
+			// vbo
+			// solid stuff from near to far
+			for( int i = 0; i < renderListSize; i++ )
+			{
+				renderList[ i ].drawSolid();
+
+				if( !renderList[ i ].isEmpty() )
+				{
+					renderedChunklets++;
+				}
+			}
+
+			GLUtil.checkGLError();
+
+			// translucent stuff from far to near
+			for( int i = renderListSize - 1; i >= 0; i-- )
+			{
+				renderList[ i ].drawTransparent();
+			}
+
+			GLUtil.checkGLError();
+		}
+
+		if( drawOutlines )
+		{
+			for( int i = 0; i < renderListSize; i++ )
+			{
+				renderList[ i ].drawOutline( renderer );
+			}
+			renderer.render();
+		}
+
+		Arrays.fill( renderList, null );
+		renderListSize = 0;
+		drawFlag++;
+	}
+
+	private void fillChunks()
+	{
+		for( int i = 0; i < chunks.length; i++ )
+		{
+			for( int j = 0; j < chunks[ i ].length; j++ )
+			{
+				final int caix = i, caiz = j;
+				final int x = chunkPosX + i - getLoadRadius();
+				final int z = chunkPosZ + j - getLoadRadius();
+
+				if( getChunk( x, z ) == null )
+				{
+					ResourceLoader.load( new ChunkLoader( this, x, z ) {
+						@Override
+						public void complete()
+						{
+							if( resource != null )
+							{
+								chunks[ caix ][ caiz ] = resource;
+
+								// need to re-evaluate the geometry of
+								// neighbouring chunks
+								Chunk c;
+								if( ( c = getChunk( x - 1, z ) ) != null )
+								{
+									c.geomDirty();
+								}
+								if( ( c = getChunk( x + 1, z ) ) != null )
+								{
+									c.geomDirty();
+								}
+								if( ( c = getChunk( x, z - 1 ) ) != null )
+								{
+									c.geomDirty();
+								}
+								if( ( c = getChunk( x, z + 1 ) ) != null )
+								{
+									c.geomDirty();
+								}
+							}
+						}
+					} );
+				}
+			}
+		}
+	}
+
+	/**
+	 * Gets a loaded chunk
+	 * 
+	 * @param x
+	 * @param z
+	 * @return the so-indexed chunk, or <code>null</code> if it does
+	 *         not exist
+	 */
+	public Chunk getChunk( int x, int z )
+	{
+		int dx = x - chunkPosX;
+		int dz = z - chunkPosZ;
+		int caix = getLoadRadius() + dx;
+		int caiz = getLoadRadius() + dz;
+
+		if( caix < 0 || caix >= chunks.length || caiz < 0 || caiz >= chunks[ caix ].length )
+		{
+			return null;
+		}
+		else
+		{
+			return chunks[ caix ][ caiz ];
+		}
+	}
+
+	/**
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @return The chunklet that contains that point
+	 */
+	public Chunklet getChunklet( float x, float y, float z )
+	{
+		Chunk chunk = getChunk( ( int ) Math.floor( x / 16 ), ( int ) Math.floor( z / 16 ) );
+
+		if( chunk != null && y >= 0 && y < 128 )
+		{
+			int yi = ( int ) Math.floor( y / 16 );
+
+			return chunk.chunklets[ yi ];
+		}
+
+		return null;
+	}
+
+	/**
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @return The type of the block that contains the point
+	 */
+	public byte blockType( float x, float y, float z )
+	{
+		Chunklet c = getChunklet( x, y, z );
+
+		if( c != null )
+		{
+			return c.parent.blockTypeForPosition( x, y, z );
+		}
+
+		return 0;
+	}
+
+	/**
+	 * @param chunkRadius
+	 */
+	@Variable( "Chunk load range" )
+	@Summary( "The distance (in chunk units) at which to load chunks" )
+	public void setLoadRadius( int chunkRadius )
+	{
+		loadradius = chunkRadius;
+
+		// I can't be bothered to work out the indices to do this
+		// properly, so brace yourself for the Madagascan strategy:
+
+		// RELOAD. EVERYTHING.
+
+		for( int i = 0; i < chunks.length; i++ )
+		{
+			for( int j = 0; j < chunks[ i ].length; j++ )
+			{
+				if( chunks[ i ][ j ] != null )
+				{
+					chunks[ i ][ j ].unload();
+				}
+			}
+		}
+		chunks = new Chunk[ 2 * chunkRadius + 1 ][ 2 * chunkRadius + 1 ];
+		fillChunks();
+	}
+
+	/**
+	 * @return chunk load radius
+	 */
+	@Variable( "Chunk load range" )
+	public int getLoadRadius()
+	{
+		return loadradius;
+	}
+
+	private static class ChunkSorter implements Comparator<Chunklet>
+	{
+		private final Vector3f eye = new Vector3f();
+
+		@Override
+		public int compare( Chunklet a, Chunklet b )
+		{
+			float ad = a.distanceSq( eye.x, eye.y, eye.z );
+			float bd = b.distanceSq( eye.x, eye.y, eye.z );
+			return ( int ) Math.signum( ad - bd );
+		}
+	}
+
+}
