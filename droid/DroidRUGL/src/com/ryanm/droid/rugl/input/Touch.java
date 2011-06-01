34ad1c740704bc40ec11cbeff57dccc2fcb5a6f3
therealryan <therealryan@gmail.com>
2011-06-01 00:38:24 +0000

    Touch is about as bulletproof as I can make it

--- droid/DroidRUGL/src/com/ryanm/droid/rugl/input/Touch.java
+++ droid/DroidRUGL/src/com/ryanm/droid/rugl/input/Touch.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import android.os.Build;
-import android.util.Log;
 import android.view.MotionEvent;
 
 import com.ryanm.droid.rugl.Game;
@@ -18,20 +16,23 @@ import com.ryanm.droid.rugl.Game;
  */
 public class Touch
 {
-	private static final boolean multitouch = Integer
-			.parseInt( Build.VERSION.SDK ) >= 5;
-
-	private static ArrayList<Pointer> pointerList = new ArrayList<Pointer>();
-
 	private static ArrayList<TouchListener> listeners =
 			new ArrayList<Touch.TouchListener>();
 
 	private static float xScale = 1, yScale = 1;
 
 	/**
-	 * An array of the active pointers
+	 * An array of pointers. They can be active or not
 	 */
-	public static Pointer[] pointers = new Pointer[0];
+	public static final Pointer[] pointers = new Pointer[8];
+	private static final boolean[] wasActive = new boolean[8];
+	static
+	{
+		for( int i = 0; i < pointers.length; i++ )
+		{
+			pointers[ i ] = new Pointer( i );
+		}
+	}
 
 	private static final Queue<MotionEvent> touchEvents =
 			new ConcurrentLinkedQueue<MotionEvent>();
@@ -49,43 +50,66 @@ public class Touch
 	 */
 	public static void processTouches()
 	{
-
 		while( !touchEvents.isEmpty() )
 		{
 			final MotionEvent me = touchEvents.poll();
 
-			if( multitouch )
-			{
-				switch( me.getAction() & MEWrap.ACTION_MASK )
+			updatePointers( me );
+
+			if( me.getAction() == MotionEvent.ACTION_UP )
+			{ // final touch has left
+				for( int i = 0; i < pointers.length; i++ )
 				{
-					case MotionEvent.ACTION_DOWN:
-						addPrimary( me );
-						break;
-					case MEWrap.ACTION_POINTER_DOWN:
-						addSecondary( me );
-						break;
-					case MotionEvent.ACTION_UP:
-						removePrimary( me );
-						break;
-					case MEWrap.ACTION_POINTER_UP:
-						removeSecondary( me );
-						break;
-					default:
-						updatePointers( me );
+					if( pointers[ i ].active )
+					{
+						pointers[ i ].active = false;
+
+						for( int j = 0; j < listeners.size(); j++ )
+						{
+							listeners.get( j ).pointerRemoved( pointers[ i ] );
+						}
+					}
 				}
 			}
-			else
+		}
+	}
+
+	private static void updatePointers( final MotionEvent me )
+	{
+		final int pointerCount = me.getPointerCount();
+
+		for( int i = 0; i < pointers.length; i++ )
+		{
+			wasActive[ i ] = pointers[ i ].active;
+			pointers[ i ].active = false;
+		}
+
+		for( int i = 0; i < pointerCount; i++ )
+		{
+			final Pointer p = pointers[ me.getPointerId( i ) ];
+
+			p.active = true;
+			p.x = me.getX( i ) * xScale;
+			p.y = ( Game.screenHeight - me.getY( i ) ) * yScale;
+			p.size = me.getSize( i );
+		}
+
+		for( int i = 0; i < pointers.length; i++ )
+		{
+			if( pointers[ i ].active && !wasActive[ i ] )
 			{
-				switch( me.getAction() )
+				// added
+				for( int j = 0; j < listeners.size(); j++ )
 				{
-					case MotionEvent.ACTION_DOWN:
-						addPrimary( me );
-						break;
-					case MotionEvent.ACTION_UP:
-						removePrimary( me );
-						break;
-					default:
-						updatePointer( me );
+					listeners.get( j ).pointerAdded( pointers[ i ] );
+				}
+			}
+			else if( !pointers[ i ].active && wasActive[ i ] )
+			{
+				// removed
+				for( int j = 0; j < listeners.size(); j++ )
+				{
+					listeners.get( j ).pointerRemoved( pointers[ i ] );
 				}
 			}
 		}
@@ -106,120 +130,6 @@ public class Touch
 	{
 		xScale = desiredWidth / actualWidth;
 		yScale = desiredHeight / actualHeight;
-	}
-
-	private static void addPrimary( final MotionEvent me )
-	{
-		assert pointerList.isEmpty();
-
-		final Pointer p = new Pointer( me.getPointerId( 0 ) );
-		p.x = me.getX() * xScale;
-		p.y = ( Game.screenHeight - me.getY() ) * yScale;
-		p.size = me.getSize();
-
-		pointerList.add( p );
-
-		pointers = pointerList.toArray( new Pointer[pointerList.size()] );
-
-		boolean eaten = false;
-		for( int j = 0; j < listeners.size() && !eaten; j++ )
-		{
-			eaten |= listeners.get( j ).pointerAdded( p );
-		}
-	}
-
-	private static void addSecondary( final MotionEvent me )
-	{
-		assert !pointerList.isEmpty();
-
-		final int pointerIndex =
-				( me.getAction() & MotionEvent.ACTION_POINTER_ID_MASK ) >> MotionEvent.ACTION_POINTER_ID_SHIFT;
-
-		final Pointer p = new Pointer( me.getPointerId( pointerIndex ) );
-		p.x = me.getX( pointerIndex ) * xScale;
-		p.y = ( Game.screenHeight - me.getY( pointerIndex ) ) * yScale;
-		p.size = me.getSize( pointerIndex );
-
-		pointerList.add( pointerIndex, p );
-
-		pointers = pointerList.toArray( new Pointer[pointerList.size()] );
-
-		boolean eaten = false;
-		for( int j = 0; j < listeners.size() && !eaten; j++ )
-		{
-			eaten |= listeners.get( j ).pointerAdded( p );
-		}
-	}
-
-	private static void removePrimary(
-			@SuppressWarnings( "unused" ) final MotionEvent me )
-	{
-		if( pointerList.size() == 1 )
-		{
-			final Pointer p = pointerList.remove( 0 );
-
-			pointers = pointerList.toArray( new Pointer[pointerList.size()] );
-
-			for( int j = 0; j < listeners.size(); j++ )
-			{
-				listeners.get( j ).pointerRemoved( p );
-			}
-		}
-		else
-		{
-			Log.e( Game.RUGL_TAG, "Touch.removePrimary() no primary to remove!" );
-		}
-	}
-
-	private static void removeSecondary( final MotionEvent me )
-	{
-		final int pointerIndex =
-				( me.getAction() & MotionEvent.ACTION_POINTER_ID_MASK ) >> MotionEvent.ACTION_POINTER_ID_SHIFT;
-
-		if( pointerList.size() > pointerIndex )
-		{
-			final Pointer p = pointerList.remove( pointerIndex );
-
-			pointers = pointerList.toArray( new Pointer[pointerList.size()] );
-
-			assert p.id == me.getPointerId( pointerIndex );
-
-			for( int j = 0; j < listeners.size(); j++ )
-			{
-				listeners.get( j ).pointerRemoved( p );
-			}
-		}
-		else
-		{
-			Log.e( Game.RUGL_TAG,
-					"Touch.removeSecondary() no pointer to remove! from index "
-							+ pointerIndex );
-		}
-	}
-
-	private static void updatePointers( final MotionEvent me )
-	{
-		final int pointerCount = MEWrap.getPointerCount( me );
-
-		for( int i = 0; i < pointerCount; i++ )
-		{
-			final Pointer p = pointerList.get( i );
-
-			assert p.id == me.getPointerId( i );
-
-			p.x = me.getX( i ) * xScale;
-			p.y = ( Game.screenHeight - me.getY( i ) ) * yScale;
-			p.size = me.getSize( i );
-		}
-	}
-
-	private static void updatePointer( final MotionEvent me )
-	{
-		final Pointer p = pointerList.get( 0 );
-
-		p.x = me.getX() * xScale;
-		p.y = ( Game.screenHeight - me.getY() ) * yScale;
-		p.size = me.getSize();
 	}
 
 	/**
@@ -259,6 +169,9 @@ public class Touch
 		/***/
 		public float size;
 
+		/***/
+		public boolean active = false;
+
 		private Pointer( final int id )
 		{
 			this.id = id;
@@ -267,10 +180,17 @@ public class Touch
 		@Override
 		public String toString()
 		{
-			final StringBuilder buff = new StringBuilder();
-			buff.append( id ).append( " ( " ).append( x ).append( ", " );
-			buff.append( y ).append( " ) " ).append( size );
-			return buff.toString();
+			if( !active )
+			{
+				return "Inactive";
+			}
+			else
+			{
+				final StringBuilder buff = new StringBuilder();
+				buff.append( id ).append( " ( " ).append( x ).append( ", " );
+				buff.append( y ).append( " ) " ).append( size );
+				return buff.toString();
+			}
 		}
 	}
 
@@ -304,33 +224,14 @@ public class Touch
 	}
 
 	/**
-	 * Wraps {@link MotionEvent} methods and variables that may not be present
-	 * 
-	 * @author ryanm
-	 */
-	private static class MEWrap
-	{
-		private static final int ACTION_MASK = MotionEvent.ACTION_MASK;
-
-		private static final int ACTION_POINTER_DOWN =
-				MotionEvent.ACTION_POINTER_DOWN;
-
-		private static final int ACTION_POINTER_UP =
-				MotionEvent.ACTION_POINTER_UP;
-
-		private static final int getPointerCount( final MotionEvent me )
-		{
-			return me.getPointerCount();
-		}
-	}
-
-	/**
 	 * Called at startup
 	 */
 	public static void reset()
 	{
-		pointerList.clear();
-		pointers = new Pointer[0];
+		for( int i = 0; i < pointers.length; i++ )
+		{
+			pointers[ i ].active = false;
+		}
 
 		for( final TouchListener l : listeners )
 		{
